162eaa35d3f6f1bae6e5fa6b6fad4af08b146cb5
Wiwin Mafiroh <wiwinmafiroh@gmail.com>
2023-08-19 23:27:22 +0700

    Implemented responsive layout & added navigation drawer

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,6 +56,36 @@ function submit() {
 let btnForm = document.getElementById("submit-form");
 btnForm.addEventListener("click", submit);
 
+//  Show Menu
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// Menu Show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// Menu Hidden
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+// Remove Menu Mobile
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
 // Change Background Header
 function scrollHeader() {
   const header = document.getElementById("header");
